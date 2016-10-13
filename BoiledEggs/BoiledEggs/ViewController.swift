4359302cb069e4f8cf41867eb0a9f01a5093ad6f
Uskinn <s.nevzor@yahoo.com>
2016-10-13 16:45:38 -0400

    first commit

---
+++ BoiledEggs/BoiledEggs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoiledEggs
+//
+//  Created by Sergey Nevzorov on 10/13/16.
+//  Copyright © 2016 Sergey Nevzorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
