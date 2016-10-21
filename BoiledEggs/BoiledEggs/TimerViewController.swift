5a446591f1b94bab0beaf1dc61c6d8d339ba9bd1
Uskinn <s.nevzor@yahoo.com>
2016-10-20 20:46:27 -0400

    timerCintroller created

---
+++ BoiledEggs/BoiledEggs/TimerViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TimerViewController.swift
+//  BoiledEggs
+//
+//  Created by Sergey Nevzorov on 10/20/16.
+//  Copyright © 2016 Sergey Nevzorov. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
